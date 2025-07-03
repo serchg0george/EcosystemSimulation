9cc5b1aecf8473562a11e9f7c668df5be4e1c5fb
Sergey Zavada <s.zavada.dev@gmail.com>
2025-07-03 09:34:01 +0300

    feat: implemented Group, Savanna classes

---
+++ src/main/java/models/animals/Group.java
@@ -0,0 +1,25 @@
+package models.animals;
+
+import java.util.List;
+
+public class Group {
+    private final String groupName;
+    private final List<Animal> groupedAnimals;
+
+    public Group(String groupName, List<Animal> groupedAnimals) {
+        this.groupName = groupName;
+        this.groupedAnimals = groupedAnimals;
+    }
+
+    private void addToGroup(Animal animal) {
+        groupedAnimals.add(animal);
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public List<Animal> getGroupedAnimals() {
+        return groupedAnimals;
+    }
+}
