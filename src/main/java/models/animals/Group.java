cbd6977b92e3c61b5792e688d9d6ba5e8fc5c17b
Sergey Zavada <s.zavada.dev@gmail.com>
2025-07-04 15:26:59 +0300

    feat: implemented addNewMember()

--- src/main/java/models/animals/Group.java
+++ src/main/java/models/animals/Group.java
@@ -1,5 +1,7 @@
 package models.animals;
 
+import enums.CarnivoreKind;
+
 import java.util.List;
 import java.util.Map;
 
@@ -11,9 +13,33 @@ public class Group {
     }
 
     public void addNewMember(Animal animal) {
+        if (isAddLonerCarnivoreSucceed(animal)) return;
+        if (isAddLonerHerbivoreSucceed(animal)) return;
         List<Animal> animals = groupedAnimals.get(animal.getGroupName());
         animals.add(animal);
         groupedAnimals.put(animal.getGroupName(), animals);
+    }
+
+    private boolean isAddLonerHerbivoreSucceed(Animal animal) {
+        if (!isCarnivore(animal) && !animal.isInGroup()) {
+            List<Animal> lonerPredators = groupedAnimals.get("Loner carnivores");
+            lonerPredators.add(animal);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isAddLonerCarnivoreSucceed(Animal animal) {
+        if (isCarnivore(animal) && !animal.isInGroup()) {
+            List<Animal> lonerPredators = groupedAnimals.get("Loner carnivores");
+            lonerPredators.add(animal);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isCarnivore(Animal animal) {
+        return animal.getAnimalKind() instanceof CarnivoreKind;
     }
 
     public Map<String, List<Animal>> getGroupedAnimals() {
