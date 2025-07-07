f9174645e123ba743ee573ec8df4a82fd794853f
Sergey Zavada <s.zavada.dev@gmail.com>
2025-07-07 14:44:46 +0300

    test: implemented tests for ProbabilitiesService

---
+++ src/test/java/services/ProbabilitiesServiceTest.java
@@ -0,0 +1,26 @@
+package services;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class ProbabilitiesServiceTest {
+    private final ProbabilitiesService service = new ProbabilitiesService();
+    
+    @Test
+    void testGetChanceForAttack_whenCalled_thenShouldBeGreaterThanZero() {
+        //given, when
+        int chance = service.getChanceForAttack();
+
+        //then
+        assertTrue(chance >= 0);
+    }
+
+    @Test
+    void testGetChanceForAttack_whenCalled_thenShouldBeLowerThan100() {
+        //given, when
+        int chance = service.getChanceForAttack();
+
+        //then
+        assertTrue(chance <= 100);
+    }
+}
