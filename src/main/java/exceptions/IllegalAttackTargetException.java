2e29fe94f29f38d10c3eca6a5033440754e2df3c
Sergey Zavada <s.zavada.dev@gmail.com>
2025-07-08 16:23:29 +0300

    feat: implemented custom exception classes

--- src/main/java/exceptions/IllegalAttackTargetException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class IllegalAttackTargetException extends RuntimeException {
-    public IllegalAttackTargetException(String message) {
-        super(message);
-    }
-}
